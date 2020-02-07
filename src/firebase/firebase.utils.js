9c0fe83be00868697179e516c86e0918baad0b68
Pavlo Sakhanda <psakhanda@gmail.com>
2020-02-07 22:06:25 +0200

    added sign-up component (with email and pass), incuded ability to store users into the firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,36 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyD6y1pvJRM20eHb5m2gSa_hFOGVPB38YhI",
-    authDomain: "crwn-db-24d8c.firebaseapp.com",
-    databaseURL: "https://crwn-db-24d8c.firebaseio.com",
-    projectId: "crwn-db-24d8c",
-    storageBucket: "crwn-db-24d8c.appspot.com",
-    messagingSenderId: "536740298705",
-    appId: "1:536740298705:web:9ddf61a9112ef1e86daaf0",
-    measurementId: "G-LJTJVQ5RYV"
+    apiKey: "AIzaSyDNY9509DSuuagT-SHpHVeqSSMedToWZ7g",
+    authDomain: "crwn-f8380.firebaseapp.com",
+    databaseURL: "https://crwn-f8380.firebaseio.com",
+    projectId: "crwn-f8380",
+    storageBucket: "crwn-f8380.appspot.com",
+    messagingSenderId: "762941553343",
+    appId: "1:762941553343:web:cce4c905891b583054240a",
+    measurementId: "G-FCKQLJMW4X"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+    if (!snapShot.exists) {
+         const { displayName, email } = userAuth;
+         const createdAt = new Date();
+         try {
+             await userRef.set({
+                 displayName,
+                 email,
+                 createdAt,
+                 ...additionalData
+             })
+         } catch (error) {
+             console.error(error)
+         }
+    }
+    return userRef;
+
 };
 
 firebase.initializeApp(config);
