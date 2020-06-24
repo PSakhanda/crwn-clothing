db3ee361de3b07fe271529ecbcbf0b8e118bb252
Pavlo Sakhanda <psakhanda@gmail.com>
2020-06-24 07:39:03 +0300

    asynchronous redux (thunk)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,11 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import logger from 'redux-logger';
-import createSagaMiddleware from 'redux-saga'
+import thunk from "redux-thunk";
 
 import rootReducer from './root-reducer';
 
-const sagaMiddleware = createSagaMiddleware();
-const middlewares = [sagaMiddleware];
+const middlewares = [thunk];
 
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger)
@@ -14,8 +13,5 @@ if (process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-// sagaMiddleware.run()
-
 export const persistor = persistStore(store);
 
-export default {store, persistStore};
